7290147a64d880133c10d26574b11a249ca90a6c
Grigorii K. Shartsev <me@shgk.me>
2024-05-14 10:16:47 +0200

    chore(vue3): @nextcloud/dialogs - replace showWarning
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/components/MessagesList/MessagesGroup/Message/Message.vue
+++ src/components/MessagesList/MessagesGroup/Message/Message.vue
@@ -398,7 +398,7 @@ export default {
 				})
 
 				if (statusCode === 202) {
-					showWarning(t('spreed', 'Message deleted successfully, but a bot or Matterbridge is configured and the message might already be distributed to other services'), {
+					window.OCP.Toast.warning(t('spreed', 'Message deleted successfully, but a bot or Matterbridge is configured and the message might already be distributed to other services'), {
 						timeout: TOAST_DEFAULT_TIMEOUT * 2,
 					})
 				} else if (statusCode === 200) {
